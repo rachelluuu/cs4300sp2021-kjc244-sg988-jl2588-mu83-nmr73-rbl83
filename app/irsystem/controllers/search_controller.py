c8e441fdc29842ed687fae1d0b3e50f5a34ffa31
joyhchen <joyhzchen@gmail.com>
2021-04-15 00:38:45 -0400

    Initialize React project, add copyfiles commands to build process so static files load correctly

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -6,9 +6,6 @@ import requests
 project_name = "Playground"
 net_id = "Michael Noor: mn598\nJoy Chen: jhc287\nJyne Dunbar: jcd322\nRachel Lu: rbl83\nVladia Trinh: vt95"
 
-@irsystem.route('/test')
-def testroute():
-  return irsystem.send_static_file('index.html')
 
 @irsystem.route('/testjson')
 def testjson():
@@ -17,17 +14,7 @@ def testjson():
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	origin = request.args.get('origin')
-	dest = request.args.get('destination')
-	vibe = request.args.get('vibe')
-	if not origin or not dest:
-		data = []
-		output_message = 'Make sure to put in an orgin and destination'
-	else:
-		output_message = "Check out these songs for your next trip from " + origin + " to " + dest + " with " + vibe
-		data = range(5) 
-		#TODO set data equal to API function calls
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+  return render_template('index.html')
 
 
 #returns list of first 100 songs from genius api for each location
