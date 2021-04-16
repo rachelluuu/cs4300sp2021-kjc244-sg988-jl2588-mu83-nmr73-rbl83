e28448ce27f3fbdf398256d847dc9ab3989dda24
joyhchen <joyhzchen@gmail.com>
2021-04-16 18:09:38 -0400

    Update client build and add whitenoise

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,12 @@ import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+from whitenoise import WhiteNoise
 
 # Configure app
 socketio = SocketIO()
 app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='app/static/')
 app.config.from_object(os.environ["APP_SETTINGS"])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
