e7e533ed5faee361ba19b25e1971982ee5010164
Joy Hangzhi Chen <jhc287@cornell.edu>
2021-04-17 13:08:46 -0400

    Move input field into different componnet

--- client/src/components/InputForm.js
+++ client/src/components/InputForm.js
@@ -1,35 +1,17 @@
+import InputGroup from './InputGroup';
+
 function InputForm(props) {
   return (
     <div className="InputForm container mx-auto lg:px-10 lg:pt-20 relative top-5 ">
       <div className="mb-3 bg-gray-50 rounded-md shadow-lg">
         <form className="px-10 py-10">
           <div className="grid gap-4 grid-cols-2">
-            <div>
-              <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-blueGray-600 bg-blueGray-200 uppercase last:mr-0 mr-1 mb-2">
-                Start Location
-              </span>
-              <input type="text" placeholder="New York" className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white bg-white rounded text-sm shadow focus:outline-none focus:ring w-full" />
-            </div>
-            <div>
-              <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-blueGray-600 bg-blueGray-200 uppercase last:mr-0 mr-1 mb-2">
-                End Location
-              </span>
-              <input type="text" placeholder="Ithaca" className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white bg-white rounded text-sm shadow focus:outline-none focus:ring w-full" />
-            </div>
+            <InputGroup label="Start Location" placeholder="New York" />
+            <InputGroup label="End Location" placeholder="Ithaca" />
           </div>
           <div className="grid gap-4 grid-cols-2 mt-10">
-            <div>
-              <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-blueGray-600 bg-blueGray-200 uppercase last:mr-0 mr-1 mb-2">
-                Vibe
-              </span>
-              <input type="text" placeholder="Groovy" className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white bg-white rounded text-sm shadow focus:outline-none focus:ring w-full" />
-            </div>
-            <div>
-              <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-blueGray-600 bg-blueGray-200 uppercase last:mr-0 mr-1 mb-2">
-                Keywords
-              </span>
-              <input type="text" placeholder="School, Class, ..." className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white bg-white rounded text-sm shadow focus:outline-none focus:ring w-full" />
-            </div>
+            <InputGroup label="Vibe" placeholder="Groovy" />
+            <InputGroup label="Keywords" placeholder="School, class..." />
           </div>
           <button onClick={props.handleClick} className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150 mt-10" type="button">
             Fetch Results
---
+++ client/src/components/InputGroup.js
@@ -0,0 +1,13 @@
+function InputGroup(props) {
+  return (
+    <div>
+      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-blueGray-600 bg-blueGray-200 uppercase last:mr-0 mr-1 mb-2">
+        {props.label}
+      </span>
+      <input type="text" placeholder={props.placeholder} className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white bg-white rounded text-sm shadow focus:outline-none focus:ring w-full" />
+    </div>
+
+  );
+}
+
+export default InputGroup;
