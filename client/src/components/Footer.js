f6deb020d296b58e4abcb3f92e8a5079fbb3859b
joyhchen <joyhzchen@gmail.com>
2021-04-24 23:31:20 -0400

    v1 update footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="mt-3 lg:px-5 lg:py-5 lg:w-2/3 mx-auto text-center">
+      <p>Team: Michael Noor (mn598), Joy Chen (jhc287), Jyne Dunbar (jcd322), Rachel Lu (rbl83), Vladia Trinh (vt95)</p>
+      <p>
+        <a className="underline" href="https://traffic-jams-v1.herokuapp.com/">Prototype 1</a> | <a className="underline" href="https://traffic-jams.herokuapp.com/">Prototype 2</a></p>
+    </div>
+  );
+}
+
+export default Footer;
