6b5ad239659265c4deec6e51e431a80e88b3f8d5
Joy Hangzhi Chen <jhc287@cornell.edu>
2021-05-03 20:11:24 -0400

    UX improvements: enforce cities in json, add required label, move keywords to top, add message to click options from dropdown

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import InputForm from './components/InputForm';
 import Results from './components/Results';
 import Footer from './components/Footer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import cities from './components/cities.json';
 
 function App() {
   const [results, setResults] = useState([]);
   const [error, setError] = useState('Type in a query to get started');
+  const [citiesList, setCitiesList] = useState(['Chicago', 'New York']);
+
+  useEffect(() => {
+    setCitiesList(cities.cities);
+  }, []);
 
   return (
     <div className="App bg-blueGray-100">
@@ -14,10 +20,13 @@ function App() {
           <h1 className="font-customStack text-4xl">
             🚦 traffic jams
           </h1>
+
+          <h2 className="text-2xl text-blueGray-600 mt-5">Generate the perfect playlist for your roadtrip.</h2>
         </div>
         <InputForm
           setResults={setResults}
           setError={setError}
+          cities={citiesList}
         />
       </div>
       <div className="-mt-5 lg:px-40 py-20">
